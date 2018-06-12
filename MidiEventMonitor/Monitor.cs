2925ed37798421a5e5fd9e9981f0fd40945741a8
spencersa <spencersa715@gmail.com>
2018-06-11 20:25:30 -0500

    Added basic monitor

---
+++ MidiEventMonitor/Monitor.cs
@@ -0,0 +1,41 @@
+﻿using NAudio.Midi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidiEventMonitor
+{
+    public interface IMonitor
+    {
+        IEnumerable<string> GetConnectedDeviceInformation()
+    }
+
+    public class Monitor : IMonitor
+    {
+        int _numberOfDevices;
+        List<MidiInCapabilities> _midiInCapabilities;
+
+        public Monitor()
+        {
+            _numberOfDevices = MidiIn.NumberOfDevices;
+            _midiInCapabilities = GetMidiInCapabilities();
+        }
+
+        public IEnumerable<string> GetConnectedDeviceInformation()
+        {
+            return _midiInCapabilities.Select(x => $"ProductId: {x.ProductId} | ProductName:{x.ProductName} | Manufacturer:{x.Manufacturer}");
+        }
+
+        private List<MidiInCapabilities> GetMidiInCapabilities()
+        {
+            var devices = new List<MidiInCapabilities>();
+            for (int i = 0; i < _numberOfDevices; i++)
+            {
+                devices.Add(MidiIn.DeviceInfo(i));
+            }
+            return devices;
+        }
+    }
+}
---
+++ MidiEventMonitor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidiEventMonitor
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            bool quit = false;
+
+            var monitor = new Monitor();
+
+            while (!quit)
+            {
+                command = Console.ReadLine().ToLower();
+                switch (command)
+                {
+                    case "-devices":
+                    case "-d":
+                        var devices = monitor.GetConnectedDeviceInformation();
+                        foreach (var device in devices)
+                        {
+                            Console.WriteLine(device);
+                        }
+                        break;
+
+                    case "-quit":
+                    case "-q":
+                        break;
+                    default:
+                        Console.WriteLine("Unknown Command " + command);
+                        break;
+                }
+            }
+        }
+    }
+}
