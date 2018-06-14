f04899c70f4601d5208c31540680c33f7e695afb
Samuel Spencer <spencersa715@gmail.com>
2018-06-13 19:05:37 -0500

    add basic monitor code

--- MidiEventMonitor/Monitor.cs
+++ MidiEventMonitor/Monitor.cs
@@ -9,23 +9,46 @@ namespace MidiEventMonitor
 {
     public interface IMonitor
     {
-        IEnumerable<string> GetConnectedDeviceInformation()
+        IEnumerable<string> GetConnectedDeviceInformation();
     }
 
     public class Monitor : IMonitor
     {
         int _numberOfDevices;
         List<MidiInCapabilities> _midiInCapabilities;
+        public List<MidiIn> MidiIns { get; set; }
+
 
         public Monitor()
         {
             _numberOfDevices = MidiIn.NumberOfDevices;
             _midiInCapabilities = GetMidiInCapabilities();
+
+            MidiIns = new List<MidiIn>();
+            for (int i = 0; i < _numberOfDevices; i++)
+            {
+                var midiIn = new MidiIn(i);
+                midiIn.MessageReceived += MidiIn_MessageReceived;
+                midiIn.ErrorReceived += MidiIn_ErrorReceived;
+                midiIn.Start();
+                MidiIns.Add(midiIn);
+            }
         }
 
         public IEnumerable<string> GetConnectedDeviceInformation()
         {
             return _midiInCapabilities.Select(x => $"ProductId: {x.ProductId} | ProductName:{x.ProductName} | Manufacturer:{x.Manufacturer}");
+        }
+
+        void MidiIn_ErrorReceived(object sender, MidiInMessageEventArgs e)
+        {
+            Logger(String.Format("Time {0} Message 0x{1:X8} Event {2}",
+                e.Timestamp, e.RawMessage, e.MidiEvent));
+        }
+
+        void MidiIn_MessageReceived(object sender, MidiInMessageEventArgs e)
+        {
+            Logger(String.Format("Time {0} Message 0x{1:X8} Event {2}", e.Timestamp, e.RawMessage, e.MidiEvent));
         }
 
         private List<MidiInCapabilities> GetMidiInCapabilities()
@@ -36,6 +59,15 @@ namespace MidiEventMonitor
                 devices.Add(MidiIn.DeviceInfo(i));
             }
             return devices;
+        }
+
+        public void Logger(string lines)
+        {
+            System.IO.StreamWriter file = new System.IO.StreamWriter("c:\\temp\\log.log", true);
+            file.WriteLine(lines);
+
+            file.Close();
+
         }
     }
 }
